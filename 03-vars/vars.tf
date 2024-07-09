0db4802bb9f838b16d0cbb19ce1835914016ff86
murali <muralikm874@gmail.com>
2024-07-09 15:27:04 +0530

    push the commit code

---
+++ 03-vars/vars.tf
@@ -0,0 +1,21 @@
+variable "sample" {
+  default = 100
+}
+
+variable "sample1" {
+  default = "Hello World"
+}
+
+output "sample" {
+  value = var.sample
+}
+
+output "sample1" {
+  value = var.sample1
+}
+
+## Sometime if variable/any reference with the combination of some other string then we have to access those in ${}
+
+output "sample-ext" {
+  value = "value of Sample - ${var.sample}"
+}
